6ec3d2c5f2cf72c899045c6fb87b3c1e5a044188
Max Jey <enjeyka@yahoo.com>
2022-09-22 13:12:23 +0300

    finished app

--- script.js
+++ script.js
@@ -5,19 +5,62 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
     createTags(e.target.value);
+
+    if (e.key === "Enter") {
+        setTimeout(() => {
+            e.target.value = "";
+        }, 10);
+        randomSelect();
+    }
 });
+
+function randomSelect() {
+    let times = 30;
+
+    let interval = setInterval(() => {
+        let randomTag = pickRandomTag();
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            let randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100);
+    }, times * 100);
+}
+
+function pickRandomTag() {
+    let tags = document.querySelectorAll(".tag");
+    return tags[Math.floor(Math.random() * tags.length)];
+}
 
 function createTags(input) {
     let tags = input
         .split(",")
         .filter((tag) => tag.trim() !== "")
         .map((tag) => tag.trim());
-    tagsEl.innerHTML = ''
+    tagsEl.innerHTML = "";
 
-    tags.forEach(tag => {
-        let tagEl = document.createElement('span')
-        tagEl.classList.add('tag')
-        tagEl.innerText = tag
-        tagsEl.appendChild(tagEl)
+    tags.forEach((tag) => {
+        let tagEl = document.createElement("span");
+        tagEl.classList.add("tag");
+        tagEl.innerText = tag;
+        tagsEl.appendChild(tagEl);
     });
+}
+
+function highlightTag(tag) {
+    tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove("highlight");
 }
